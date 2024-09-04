625ee993dfc54dc19a7894c03224d6e8c5327fd8
Haemoffy <fuwansuu@gmail.com>
2024-09-05 00:00:00 +0800

    added btn to lead them to post creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import PostList from './PostList'
+import CreatePost from './CreatePost'
 
 export default function App() {
   const [currentPost, setCurrentPost] = useState(<PostList id={ 1 } />)
@@ -10,6 +11,11 @@ export default function App() {
         <button onClick={ () => setCurrentPost(<PostList id={ 1 } />) }>Post 1</button>
         <button onClick={ () => setCurrentPost(<PostList id={ 2 } />) }>Post 2</button>
         <button onClick={ () => setCurrentPost(<PostList id={ 3 } />) }>Post 3</button>
+        <button 
+          onClick={ () => setCurrentPost(<CreatePost setCurrentPost={ setCurrentPost } />) }
+          >
+            Create Post
+        </button>
         { currentPost }
       </div>
     </>
