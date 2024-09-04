306b05dc00b7a9ad7912f9202f79d38381d90ff7
Haemoffy <fuwansuu@gmail.com>
2024-09-04 23:57:36 +0800

    added function for podst creation

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,4 +1,19 @@
-export default function getPosts(id) {
+export function getPost(id) {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => res.json())
+}
+
+export function createPost({ title, body }) {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            userId: 1,
+            title,
+            body
+        })
+    })
+    .then(res => res.json())
 }
